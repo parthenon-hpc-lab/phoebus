60305590520016d48137dfa4db89b6fa6ad2aa1b
Logan Alexandria White <lawhite@wentletrap.hpc.arizona.edu>
2026-02-23 22:29:10 -0700

    updated sie + eos calculations/methods

--- scripts/python/grsolver/seos.py
+++ scripts/python/grsolver/seos.py
@@ -1,4 +1,4 @@
-from singularity_eos import StellarCollapse, thermalqs
+from singularity_eos import StellarCollapse, thermalqs, Helmholtz
 import numpy as np
 import math
 import matplotlib.pyplot as pl
@@ -16,7 +16,7 @@ def FindTemperature(rho, p, lmbda, tmin, tmax, eos):
     return T
 
 
-def CalculateInternalEnergy(rho, ye, p, tmin, tmax, filename):
+def CalculateInternalEnergy_DEPR(rho, ye, p, tmin, tmax, filename):
     eos1 = StellarCollapse(filename, use_sp5=False, filter_bmod=True)
     nlambda = eos1.nlambda  # get number of elements per lambda
     lmbda = np.zeros(nlambda, dtype=np.double)
@@ -29,3 +29,34 @@ def CalculateInternalEnergy(rho, ye, p, tmin, tmax, filename):
         eps[i] = eos1.InternalEnergyFromDensityTemperature(rho[i], T[i], lmbda)
         u[i] = eps[i] * rho[i]
     return eps, u, T
+
+def CalculateInternalEnergy_StellarCollapse(rho, T, ye, filename, verbose=False):
+    eos1 = StellarCollapse(filename, use_sp5=False, filter_bmod=True)
+    nlambda = eos1.nlambda  # get number of elements per lambda
+    lmbda = np.zeros(nlambda, dtype=np.double)
+    u = np.zeros(len(rho))
+    eps = np.zeros(len(rho))
+
+    for i in range(len(rho)):
+        lmbda[0] = ye[i]
+        eps[i] = eos1.InternalEnergyFromDensityTemperature(rho[i], T[i], lmbda)
+        u[i] = eps[i] * rho[i]
+
+    # if verbose: print(nlambda, lmbda)
+    return eps, u
+
+def CalculateInternalEnergy_Helmholtz(rho, T, abar, zbar, filename='/home/u1/lawhite/singularity-eos/data/helmholtz/helm_table.dat', verbose=False):
+    eos1 = Helmholtz(filename)
+    nlambda = eos1.nlambda  # get number of elements per lambda
+    lmbda = np.zeros(nlambda, dtype=np.double)
+    u = np.zeros(len(rho))
+    eps = np.zeros(len(rho))
+
+    for i in range(len(rho)):
+        lmbda[0] = abar[i]
+        lmbda[1] = zbar[i]
+        eps[i] = eos1.InternalEnergyFromDensityTemperature(rho[i], T[i], lmbda)
+        u[i] = eps[i] * rho[i]
+
+    # if verbose: print(nlambda, lmbda)
+    return eps, u
