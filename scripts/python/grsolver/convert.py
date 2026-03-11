corrected temperature scaling

scripts/python/grsolver/convert.py
@@ -12,6 +12,7 @@ from argparse import ArgumentParser
 
 G = const.G.cgs.value
 c = const.c.cgs.value
+kB = const.k_B.cgs.value
 msun = const.M_sun.cgs.value
 
 
@@ -25,8 +26,9 @@ def ConvertData(filename, formattype):
     newdata["r"] = data["r"] * c ** 2.0 / G / M0
     # np.save('r',newdata['r'])
     newdata["mass_density"] = data["mass_density"] * (G / c ** 2.0) ** 3.0 * M0 ** 2.0
-    newdata["temp"] = data["temp"]
+    newdata["temp"] = data["temp"] * kB
     newdata["Ye"] = data["Ye"]
+    newdata["velocity"] = data["velocity"] / c
     newdata["specific_internal_energy"] = data["specific_internal_energy"] / c ** 2.0
     newdata["pressure"] = data["pressure"] * G ** 3.0 / c ** 8.0 * M0 ** 2.0
     dvdr = (newdata["velocity"][1:] - newdata["velocity"][:-1]) / (